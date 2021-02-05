7cfcd40149a4469669ae61fbff91dd9b165a7398
Jhonny Fialho <jhonny@hello-world.cc>
2021-02-05 01:02:13 -0300

    change

--- resources/views/modify/ajax/change.blade.php
+++ resources/views/modify/ajax/change.blade.php
@@ -2,10 +2,10 @@
     $id = date('hisu');
 @endphp
 <div class="card mb-3">
+    <div class="card-header text-end">
+        <button class="btn btn-primary btn-sm btn-copy btn-clipboard" data-clipboard-text="{!! nl2br($text) !!}">copiar</button>
+    </div>
     <div class="card-body">
-        <p class="text-end">
-            <button class="btn btn-primary btn-sm btn-copy" data-clipboard-text="{!! nl2br($text) !!}">copiar</button>
-        </p>        
         <p class="card-text">{!! nl2br($text) !!}</p>
     </div>
 </div>
